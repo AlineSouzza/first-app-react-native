6c5c43882ef56cfdb37aae866bec7dab389d7bbd
Aline Souza <alinesouza.hrzon@gmail.com>
2022-10-06 15:13:42 -0300

    first commit

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native"
+
+export default function Title() {
+    return (
+        <View>
+            <Text>ONEBITHEALTH</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+})
+
